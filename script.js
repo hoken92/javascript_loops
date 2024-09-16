bcc5f07d19f23ddc290992d16070d95cdc3779f4
hoken92 <hkenneth92@gmail.com>
2024-09-15 19:28:02 -0700

    added part 3 solution

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ let number = 1;
 
 while (number <= 100) {
   if (number % 3 == 0 && number % 5 === 0) {
-    // console.log("Fizz buzz");
+    console.log("Fizz buzz");
   } else if (number % 3 === 0) {
-    // console.log("Fizz");
+    console.log("Fizz");
   } else if (number % 5 === 0) {
-    // console.log("Buzz");
+    console.log("Buzz");
   } else {
-    // console.log(number);
+    console.log(number);
   }
   number++;
 }
@@ -89,21 +89,22 @@ Be careful! If you set n to a number too large, your loop could take a long time
 const data =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-let cell1;
-let cell2;
-let cell3;
-let cell4;
+const data2 =
+  "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
 // Iterates through each letter of the string
 
-// for (const i in data) {
+let cell = "";
 
-//   if (data[i] === ",") {
-//     cell[1] = data[i]
-//   }
+for (let i = 0; i < data.length; i++) {
+  cell += data[i];
 
-//   // Looking for "," to ignore and move to the next letter
-
-//   // Need to look for \n and ignore it
-//   // Only four cells per row
-// }
+  if (data[i] == ",") {
+    // Includes a space after there is a comma
+    cell += " ";
+  } else if (data[i] == "\n") {
+    // Enters new line after there is a \n
+    cell += "\n";
+  }
+}
+console.log(cell);
